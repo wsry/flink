bffbb474bddb95e10c428445129330e9f0d1c8eb
kevin.cyj <kevin.cyj@alibaba-inc.com>
2020-08-31 15:30:20 +0800

    Introduce BufferWritingResultPartition and migrate logics related to ResultSubpartition from ResultPartition to it

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/PipelinedResultPartition.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/PipelinedResultPartition.java
@@ -46,7 +46,7 @@ import static org.apache.flink.util.Preconditions.checkArgument;
  * {@link #onConsumedSubpartition(int)}) then the partition as a whole is disposed and all buffers are
  * freed.
  */
-public class PipelinedResultPartition extends ResultPartition implements CheckpointedResultPartition {
+public class PipelinedResultPartition extends BufferWritingResultPartition implements CheckpointedResultPartition {
 
 	/** The lock that guard release operations (which can be asynchronously propagated from the
 	 * networks threads. */
@@ -123,12 +123,12 @@ public class PipelinedResultPartition extends ResultPartition implements Checkpo
 
 	@Override
 	public CheckpointedResultSubpartition getCheckpointedSubpartition(int subpartitionIndex) {
-		return (CheckpointedResultSubpartition) getAllPartitions()[subpartitionIndex];
+		return (CheckpointedResultSubpartition) subpartitions[subpartitionIndex];
 	}
 
 	@Override
 	public void readRecoveredState(ChannelStateReader stateReader) throws IOException, InterruptedException {
-		for (ResultSubpartition subPar : getAllPartitions()) {
+		for (ResultSubpartition subPar : subpartitions) {
 			((PipelinedSubpartition) subPar).readRecoveredState(stateReader);
 		}
 
