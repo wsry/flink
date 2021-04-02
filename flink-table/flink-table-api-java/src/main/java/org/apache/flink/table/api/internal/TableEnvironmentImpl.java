326499bc476a1b452b079905ec8506dce7cd2f99
kevin.cyj <kevin.cyj@alibaba-inc.com>
2021-04-02 10:54:39 +0800

    Test4

--- flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/api/internal/TableEnvironmentImpl.java
+++ flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/api/internal/TableEnvironmentImpl.java
@@ -301,6 +301,7 @@ public class TableEnvironmentImpl implements TableEnvironmentInternal {
                 new FunctionCatalog(tableConfig, catalogManager, moduleManager);
 
         Map<String, String> executorProperties = settings.toExecutorProperties();
+        executorProperties.putAll(configuration.toMap());
         Executor executor =
                 ComponentFactoryService.find(ExecutorFactory.class, executorProperties)
                         .create(executorProperties);
