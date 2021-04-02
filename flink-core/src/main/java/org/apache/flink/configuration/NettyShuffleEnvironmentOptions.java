12bee254ed0c725c9444d94f0e06a7b528a1cfec
kevin.cyj <kevin.cyj@alibaba-inc.com>
2021-04-02 13:26:20 +0800

    Test8

--- flink-core/src/main/java/org/apache/flink/configuration/NettyShuffleEnvironmentOptions.java
+++ flink-core/src/main/java/org/apache/flink/configuration/NettyShuffleEnvironmentOptions.java
@@ -205,7 +205,7 @@ public class NettyShuffleEnvironmentOptions {
     public static final ConfigOption<Integer> NETWORK_SORT_SHUFFLE_MIN_PARALLELISM =
             key("taskmanager.network.sort-shuffle.min-parallelism")
                     .intType()
-                    .defaultValue(1)
+                    .defaultValue(Integer.MAX_VALUE)
                     .withDescription(
                             "Parallelism threshold to switch between sort-merge blocking shuffle "
                                     + "and the default hash-based blocking shuffle, which means for small parallelism,"
